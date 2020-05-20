ffbc0cc7ec4c103f0107e8773af99bccc1e7249c
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-05-20 13:16:58 -0400

    add some dat to chart

--- script.js
+++ script.js
@@ -5,24 +5,18 @@ google.charts.setOnLoadCallback(drawSeriesChart);
 function drawSeriesChart() {
 
   var data = google.visualization.arrayToDataTable([
-    ['ID', 'Life Expectancy', 'Fertility Rate', 'Region',     'Population'],
-    ['CAN',    80.66,              1.67,      'North America',  33739900],
-    ['DEU',    79.84,              1.36,      'Europe',         81902307],
-    ['DNK',    78.6,               1.84,      'Europe',         5523095],
-    ['EGY',    72.73,              2.78,      'Middle East',    79716203],
-    ['GBR',    80.05,              2,         'Europe',         61801570],
-    ['IRN',    72.49,              1.7,       'Middle East',    73137148],
-    ['IRQ',    68.09,              4.77,      'Middle East',    31090763],
-    ['ISR',    81.55,              2.96,      'Middle East',    7485600],
-    ['RUS',    68.6,               1.54,      'Europe',         141850000],
-    ['USA',    78.09,              2.05,      'North America',  307007000]
+    ['ID',    'Age',        'Impact',         'How',       'Enjoyment'],
+    ['Robbed',    4,              5,      'Out of her control',  33739900],
+    ['Married',    25,              5,      'A choice',         81902307],
+    ['Motorcycle',    32,               2,      'A choice',         5523095],
+    ['Skydive',    27,              3,      'A choice',    79716203],
   ]);
 
   let options = {
-    title: 'Correlation between life expectancy, fertility rate ' +
-           'and population of some world countries (2010)',
-    hAxis: {title: 'Life Expectancy'},
-    vAxis: {title: 'Fertility Rate'},
+    title: 'Correlation between age, impact,' +
+           'and enjoyment of Zoë\'s life experiences',
+    hAxis: {title: 'Age'},
+    vAxis: {title: 'Impact'},
     bubble: {textStyle: {fontSize: 11}}      };
 
   let chart = new google.visualization.BubbleChart(document.getElementById('chartArea'));
