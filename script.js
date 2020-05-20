487514030783833fe223ac0dd2697d49f38b7cda
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-05-20 15:26:20 -0400

    style to chart - axis

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 google.charts.load('current', {'packages':['corechart']});
-
 google.charts.setOnLoadCallback(drawSeriesChart);
 
 function drawSeriesChart() {
-
-  var data = google.visualization.arrayToDataTable([
+  let data = google.visualization.arrayToDataTable([
     ['ID',              'Age',    'Impact',      'How',             'Enjoyment'],
     ['Appendix',          16,       1,      'Out of my control',        3],
     ['Got Robbed',        4,        5,      'Out of my control',        1],
@@ -20,16 +18,30 @@ function drawSeriesChart() {
     ['Bought 1st car',    24,       1,      'A choice',                 2],
     ['1st Apartment',     19,       3,      'A choice',                 3],
     ['#Vanlife',          37,       5,      'A choice',                 5],
-    [ ,                45,       1,      'A choice',                 1],
   ]);
 
   let options = {
-    title: 'Correlation between age, impact,' +
-           'and enjoyment of major life experiences',
+    height: 800,
     hAxis: {title: 'Age'},
-    vAxis: {title: 'Impact'},
-    bubble: {textStyle: {fontSize: 11}}      };
+    vAxis: {
+      title: 'Impact',
+      maxValue: 6
+    },
+    bubble: {
+      textStyle: {
+        fontSize: 11,
+        auraColor: 'none'
+      }
+    },
+          
+  };
 
   let chart = new google.visualization.BubbleChart(document.getElementById('chartArea'));
   chart.draw(data, options);
+
+  
 }
+
+$(window).resize(function(){
+  drawSeriesChart();
+});
