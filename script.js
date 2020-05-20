52dd7d7c827103284bdf46e139221921756f5347
zsiskos <43017972+zsiskos@users.noreply.github.com>
2020-05-20 13:44:55 -0400

    some layout and add image

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ google.charts.setOnLoadCallback(drawSeriesChart);
 function drawSeriesChart() {
 
   var data = google.visualization.arrayToDataTable([
-    ['ID',    'Age',        'Impact',         'How',       'Enjoyment'],
-    ['Robbed',    4,              5,      'Out of her control',  33739900],
-    ['Married',    25,              5,      'A choice',         81902307],
-    ['Motorcycle',    32,               2,      'A choice',         5523095],
-    ['Skydive',    27,              3,      'A choice',    79716203],
+    ['ID',        'Age',    'Impact',      'How',              'Enjoyment'],
+    ['Robbed',       4,        5,      'Out of my control',       1],
+    ['Married',      25,       5,      'A choice',                 7],
+    ['Motorcycle',   32,       2,      'A choice',                 7],
+    ['Skydive',      27,       3,      'A choice',                10],
   ]);
 
   let options = {
     title: 'Correlation between age, impact,' +
-           'and enjoyment of Zoë\'s life experiences',
+           'and enjoyment of major life experiences',
     hAxis: {title: 'Age'},
     vAxis: {title: 'Impact'},
     bubble: {textStyle: {fontSize: 11}}      };
